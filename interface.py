686704687c72aca3ec8e5ae70a0e583d59ba4e15
Mattia <mattiabacigalupo@gmail.com>
2025-05-24 11:26:57 +0200

    v.0.0
    Implementation of the QuantumDevice class

--- interface.py
+++ interface.py
@@ -1,11 +1,26 @@
 # Libraries needed 
-from abc import ABCMeta
+from abc import ABCMeta, ABC, abstractmethod
+from contextlib import contextmanager
 
 # Defines the basic functions a quantum computing program must have 
 class QuantumDevice(metaclass=ABCMeta):
   @abstractmethod
-  def allocate_qubit(self)->Qubit:
+  def allocate_qubit(self) -> Qubit: 
     pass 
-
+  
   @abstractmethod
+  def deallocate_qubit(self) -> Qubit: 
+    pass 
+  
+  @contextmanager
+  def using_qubit(self):
+    qubit = self.allocate_qubit() 
+    try:
+      yield qubit 
+    finally:
+      qubit.reset()
+      self.deallocate_qubit() 
+
+      
+
   

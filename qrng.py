6be9c4eaf16a55bf5fa52a4a346c6c9eed92c857
Mattia <mattiabacigalupo@gmail.com>
2025-05-24 16:14:14 +0200

    v.0.1
    Simulator and QRNG programs added

---
+++ qrng.py
@@ -0,0 +1,27 @@
+# Mattia Bacigalupo, 2025/05/24
+# Creation of random states of the qubit using a quantum computer generator
+# Reference to the book "Learn Quantum Computing with Python and Q#" by
+# Sarah Kaiser and Cassandra Granade, published by Manning Publications Co.
+# Book ISBN 9781617296130.
+
+import interface as irf 
+import simulator as sim 
+
+def qrng(device: irf.QuantumDevice) -> bool:
+    with device.using_qubit() as q:
+        q.h() 
+        return q.measure() 
+    
+# Main program 
+if __name__ == "__main__":
+    qsim = sim.SingleQubitSimulator()
+    for i in range(100):
+        random_sample = qrng(qsim)
+        print(f"Our quantum random number generator returned:\n{random_sample}")
+
+        
+
+
+
+
+
---
+++ simulator.py
@@ -0,0 +1,42 @@
+# Libraries needed
+import numpy as np 
+import interface as irf 
+
+# Setting of the main vectors
+KET_0 = np.array([
+    [1],
+    [0]
+    ], dtype = complex) 
+
+H = np.array([
+    [1, 1],
+    [1, -1]
+    ], dtype = complex)/np.sqrt(2) 
+
+# Building the classes
+class SimulatedQubit(irf.Qubit):
+    def __init__(self):
+        self.reset()
+
+    def h(self):
+        self.state = H @ self.state 
+
+    def measure(self) -> bool:
+        pr0 = np.abs(self.state[0, 0]) ** 2
+        sample = np.random.random() <= pr0 
+        return bool(0 if sample else 1)
+    
+    def reset(self):
+        self.state = KET_0.copy() 
+
+class SingleQubitSimulator(irf.QuantumDevice):
+    available_qubits = [SimulatedQubit()]
+
+    def allocate_qubit(self) -> SimulatedQubit:
+        if self.available_qubits:
+            return self.available_qubits.pop() 
+        
+    def deallocate_qubit(self, qubit:SimulatedQubit):
+        self.available_qubits.append(qubit)
+
+        
